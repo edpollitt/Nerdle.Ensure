9ae5b49827266453df09b7b9aa245d42ef4cba44
Ed Pollitt <ed.poll@justgiving.com>
2015-05-14 19:00:33 +0100

    Added Between conditions

---
+++ Nerdle.Ensure.Tests/When_ensuring_between.cs
@@ -0,0 +1,145 @@
+using System;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Nerdle.Ensure.Tests
+{
+    [TestFixture]
+    public class When_ensuring_between
+    {
+        [TestFixture]
+        public class On_a_value
+        {
+            [Test]
+            public void No_exception_is_thrown_if_the_value_is_equal_to_the_lower_bound()
+            {
+                Action ensuring = () => Ensure.Value(1).Between(1, 100);
+                ensuring.ShouldNotThrow();
+            }
+
+            [Test]
+            public void No_exception_is_thrown_if_the_value_is_equal_to_the_upper_bound()
+            {
+                Action ensuring = () => Ensure.Value(100).Between(1, 100);
+                ensuring.ShouldNotThrow();
+            }
+
+            [Test]
+            public void No_exception_is_thrown_if_the_value_is_between_the_lower_and_upper_bounds()
+            {
+                Action ensuring = () => Ensure.Value(0).Between(-1, 1);
+                ensuring.ShouldNotThrow();
+            }
+
+            [Test]
+            public void An_exception_is_thrown_if_the_value_is_less_than_the_lower_bound()
+            {
+                Action ensuring = () => Ensure.Value(0).Between(1, 10);
+                ensuring.ShouldThrow<Exception>().WithMessage("Value must be between 1 and 10 but was 0.");
+            }
+
+            [Test]
+            public void An_exception_is_thrown_if_the_value_is_greater_than_the_upper_bound()
+            {
+                Action ensuring = () => Ensure.Value(11).Between(1, 10);
+                 ensuring.ShouldThrow<Exception>().WithMessage("Value must be between 1 and 10 but was 11.");
+            }
+
+            [Test]
+            public void An_InvalidOperationException_is_thrown_by_default()
+            {
+                Action ensuring = () => Ensure.Value(1).Between(-1, 0);
+                ensuring.ShouldThrowExactly<InvalidOperationException>();
+            }
+
+            [Test]
+            public void A_custom_message_can_be_specified()
+            {
+                Action ensuring = () => Ensure.Value(1).Between(-1, 0, "foo");
+                ensuring.ShouldThrowExactly<InvalidOperationException>().WithMessage("foo");
+            }
+
+            [Test]
+            public void A_custom_exception_can_be_specified()
+            {
+                Action ensuring = () => Ensure.Value(1).Between(10, 100, _ => new IndexOutOfRangeException("bar"));
+                ensuring.ShouldThrowExactly<IndexOutOfRangeException>().WithMessage("bar");
+            }
+
+            [Test]
+            public void The_ensurable_is_returned()
+            {
+                var theEnsurable = Ensure.Value(1);
+                theEnsurable.Between(0, 1).Should().Be(theEnsurable);
+            }
+        }
+
+        [TestFixture]
+        public class On_an_argument
+        {
+            [Test]
+            public void No_exception_is_thrown_if_the_value_is_equal_to_the_lower_bound()
+            {
+                Action ensuring = () => Ensure.Argument(1).Between(1, 100);
+                ensuring.ShouldNotThrow();
+            }
+
+            [Test]
+            public void No_exception_is_thrown_if_the_value_is_equal_to_the_upper_bound()
+            {
+                Action ensuring = () => Ensure.Argument(100).Between(1, 100);
+                ensuring.ShouldNotThrow();
+            }
+
+            [Test]
+            public void No_exception_is_thrown_if_the_value_is_between_the_lower_and_upper_bounds()
+            {
+                Action ensuring = () => Ensure.Argument(0).Between(-1, 1);
+                ensuring.ShouldNotThrow();
+            }
+
+            [Test]
+            public void An_exception_is_thrown_if_the_value_is_less_than_the_lower_bound()
+            {
+                Action ensuring = () => Ensure.Argument(0).Between(1, 10);
+                ensuring.ShouldThrow<Exception>().WithMessage("Value must be between 1 and 10 but was 0.");
+            }
+
+            [Test]
+            public void An_exception_is_thrown_if_the_value_is_greater_than_the_upper_bound()
+            {
+                Action ensuring = () => Ensure.Argument(11).Between(1, 10);
+                ensuring.ShouldThrow<Exception>().WithMessage("Value must be between 1 and 10 but was 11.");
+            }
+
+            [Test]
+            public void An_ArgumentOutOfRangeException_is_thrown_by_default()
+            {
+                Action ensuring = () => Ensure.Argument(1).Between(-1, 0);
+                ensuring.ShouldThrowExactly<ArgumentOutOfRangeException>();
+            }
+
+            [Test]
+            public void A_custom_message_can_be_specified()
+            {
+                Action ensuring = () => Ensure.Argument(1).Between(-1, 0, "foo");
+                ensuring.ShouldThrowExactly<ArgumentOutOfRangeException>().WithMessage("foo");
+            }
+
+            [Test]
+            public void A_custom_exception_can_be_specified()
+            {
+                Action ensuring = () => Ensure.Argument(1).Between(10, 100, _ => new FileLoadException("bar"));
+                ensuring.ShouldThrowExactly<FileLoadException>().WithMessage("bar");
+            }
+
+            [Test]
+            public void The_ensurable_is_returned()
+            {
+                var theEnsurable = Ensure.Argument(1);
+                theEnsurable.Between(0, 1).Should().Be(theEnsurable);
+            }
+        }
+    }
+}
---
+++ Nerdle.Ensure/Extensions/BetweenExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Nerdle.Ensure
+{
+    [DebuggerStepThrough]
+    public static class BetweenExtensions
+    {
+        public static Ensurable<T> Between<T>(this Ensurable<T> ensurable, T lower, T upper,
+            string exceptionMessage = null) where T : IComparable<T>
+        {
+            return ensurable.Satisfies(x => x.CompareTo(lower) > -1 && x.CompareTo(upper) < 1,
+                exceptionMessage ?? string.Format("Value must be between {0} and {1} but was {2}.", lower, upper, ensurable));
+        }
+
+        public static Ensurable<T> Between<T>(this Ensurable<T> ensurable, T lower, T upper,
+            Func<Ensurable<T>, Exception> exceptionFactory) where T : IComparable<T>
+        {
+            return ensurable.Satisfies(x => x.CompareTo(lower) > -1 && x.CompareTo(upper) < 1, exceptionFactory);
+        }
+
+        public static Ensurable<T> Between<T>(this EnsurableArgument<T> ensurable, T lower, T upper,
+            string exceptionMessage = null) where T : IComparable<T>
+        {
+            return ensurable.Satisfies(x => x.CompareTo(lower) > -1 && x.CompareTo(upper) < 1,
+                e => new ArgumentOutOfRangeException(ensurable.Name, 
+                    exceptionMessage ?? string.Format("Value must be between {0} and {1} but was {2}.", lower, upper, ensurable)));
+        } 
+    }
+}
