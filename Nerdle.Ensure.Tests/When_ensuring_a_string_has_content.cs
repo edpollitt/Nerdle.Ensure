7ff318b2b34c29d556b6bc5728c517c5ede2f5f7
edpollitt <edpollitt@gmail.com>
2015-06-29 20:33:07 +0100

    Renamed HasContent to NotBlank

--- Nerdle.Ensure.Tests/When_ensuring_a_string_has_content.cs
+++ Nerdle.Ensure.Tests/When_ensuring_a_string_has_content.cs
@@ -5,57 +5,57 @@ using NUnit.Framework;
 namespace Nerdle.Ensure.Tests
 {
     [TestFixture]
-    public class When_ensuring_a_string_has_content
+    public class When_ensuring_a_string_is_not_blank
     {
         [TestFixture]
         public class On_a_value
         {
             [Test]
-            public void No_exception_is_thrown_if_the_string_has_content()
+            public void No_exception_is_thrown_if_the_string_is_not_blank()
             {
-                Action ensuring = () => Ensure.ValueOf("foo").HasContent();
+                Action ensuring = () => Ensure.ValueOf("foo").NotBlank();
                 ensuring.ShouldNotThrow();
             }
 
             [Test]
             public void An_exception_is_thrown_if_the_string_is_null()
             {
-                Action ensuring = () => Ensure.ValueOf((string)null).HasContent();
+                Action ensuring = () => Ensure.ValueOf((string)null).NotBlank();
                 ensuring.ShouldThrow<Exception>().WithMessage("Cannot be null or white space.");
             }
 
             [Test]
             public void An_exception_is_thrown_if_the_string_is_empty()
             {
-                Action ensuring = () => Ensure.ValueOf(string.Empty).HasContent();
+                Action ensuring = () => Ensure.ValueOf(string.Empty).NotBlank();
                 ensuring.ShouldThrow<Exception>().WithMessage("Cannot be null or white space.");
             }
 
             [Test]
             public void An_exception_is_thrown_if_the_string_is_white_space()
             {
-                Action ensuring = () => Ensure.ValueOf("   ").HasContent();
+                Action ensuring = () => Ensure.ValueOf("   ").NotBlank();
                 ensuring.ShouldThrow<Exception>().WithMessage("Cannot be null or white space.");
             }
 
             [Test]
             public void The_default_exception_is_InvalidOperationException()
             {
-                Action ensuring = () => Ensure.ValueOf(string.Empty).HasContent();
+                Action ensuring = () => Ensure.ValueOf(string.Empty).NotBlank();
                 ensuring.ShouldThrowExactly<InvalidOperationException>().WithMessage("Cannot be null or white space.");
             }
 
             [Test]
             public void A_custom_message_can_be_specified()
             {
-                Action ensuring = () => Ensure.ValueOf(string.Empty).HasContent("foo");
+                Action ensuring = () => Ensure.ValueOf(string.Empty).NotBlank("foo");
                 ensuring.ShouldThrowExactly<InvalidOperationException>().WithMessage("foo");
             }
 
             [Test]
             public void A_custom_exception_can_be_specified()
             {
-                Action ensuring = () => Ensure.ValueOf(string.Empty).HasContent(_ => new IndexOutOfRangeException("bar"));
+                Action ensuring = () => Ensure.ValueOf(string.Empty).NotBlank(_ => new IndexOutOfRangeException("bar"));
                 ensuring.ShouldThrowExactly<IndexOutOfRangeException>().WithMessage("bar");
             }
 
@@ -63,7 +63,7 @@ namespace Nerdle.Ensure.Tests
             public void The_ensurable_is_returned()
             {
                 var theEnsurable = Ensure.ValueOf("hello");
-                theEnsurable.HasContent().Should().Be(theEnsurable);
+                theEnsurable.NotBlank().Should().Be(theEnsurable);
             }
         }
 
@@ -71,65 +71,65 @@ namespace Nerdle.Ensure.Tests
         public class On_an_argument
         {
             [Test]
-            public void No_exception_is_thrown_if_the_string_has_content()
+            public void No_exception_is_thrown_if_the_string_is_not_blank()
             {
-                Action ensuring = () => Ensure.Argument("foo").HasContent();
+                Action ensuring = () => Ensure.Argument("foo").NotBlank();
                 ensuring.ShouldNotThrow();
             }
 
             [Test]
             public void An_exception_is_thrown_if_the_string_is_null()
             {
-                Action ensuring = () => Ensure.Argument((string)null).HasContent();
+                Action ensuring = () => Ensure.Argument((string)null).NotBlank();
                 ensuring.ShouldThrow<Exception>().WithMessage("Cannot be null or white space.");
             }
 
             [Test]
             public void An_exception_is_thrown_if_the_string_is_empty()
             {
-                Action ensuring = () => Ensure.Argument(string.Empty).HasContent();
+                Action ensuring = () => Ensure.Argument(string.Empty).NotBlank();
                 ensuring.ShouldThrow<Exception>().WithMessage("Cannot be null or white space.");
             }
 
             [Test]
             public void An_exception_is_thrown_if_the_string_is_white_space()
             {
-                Action ensuring = () => Ensure.Argument("   ").HasContent();
+                Action ensuring = () => Ensure.Argument("   ").NotBlank();
                 ensuring.ShouldThrow<Exception>().WithMessage("Cannot be null or white space.");
             }
 
             [Test]
             public void The_default_exception_is_ArgumentException()
             {
-                Action ensuring = () => Ensure.Argument(string.Empty).HasContent();
+                Action ensuring = () => Ensure.Argument(string.Empty).NotBlank();
                 ensuring.ShouldThrowExactly<ArgumentException>().WithMessage("Cannot be null or white space.");
             }
 
             [Test]
             public void A_custom_message_can_be_specified()
             {
-                Action ensuring = () => Ensure.Argument(string.Empty).HasContent("foo");
+                Action ensuring = () => Ensure.Argument(string.Empty).NotBlank("foo");
                 ensuring.ShouldThrowExactly<ArgumentException>().WithMessage("foo");
             }
 
             [Test]
             public void The_exception_includes_the_name_if_set()
             {
-                Action ensuring = () => Ensure.Argument(string.Empty, "myArg").HasContent("foo");
+                Action ensuring = () => Ensure.Argument(string.Empty, "myArg").NotBlank("foo");
                 ensuring.ShouldThrowExactly<ArgumentException>().And.ParamName.Should().Be("myArg");
             }
 
             [Test]
             public void The_exception_does_not_include_the_name_if_not_set()
             {
-                Action ensuring = () => Ensure.Argument(string.Empty).HasContent("foo");
+                Action ensuring = () => Ensure.Argument(string.Empty).NotBlank("foo");
                 ensuring.ShouldThrowExactly<ArgumentException>().And.ParamName.Should().BeNull();
             }
 
             [Test]
             public void A_custom_exception_can_be_specified()
             {
-                Action ensuring = () => Ensure.Argument(string.Empty).HasContent(_ => new IndexOutOfRangeException("bar"));
+                Action ensuring = () => Ensure.Argument(string.Empty).NotBlank(_ => new IndexOutOfRangeException("bar"));
                 ensuring.ShouldThrowExactly<IndexOutOfRangeException>().WithMessage("bar");
             }
 
@@ -137,7 +137,7 @@ namespace Nerdle.Ensure.Tests
             public void The_ensurable_is_returned()
             {
                 var theEnsurable = Ensure.Argument("hello");
-                theEnsurable.HasContent().Should().Be(theEnsurable);
+                theEnsurable.NotBlank().Should().Be(theEnsurable);
             }
         }
     }
--- Nerdle.Ensure/Extensions/NotBlankExtensions.cs
+++ Nerdle.Ensure/Extensions/NotBlankExtensions.cs
@@ -4,14 +4,14 @@ using System.Diagnostics;
 namespace Nerdle.Ensure
 {
     [DebuggerStepThrough]
-    public static class HasContentExtensions
+    public static class NotBlankExtensions
     {
-        public static Ensurable<string> HasContent(this Ensurable<string> ensurable, string exceptionMessage = null)
+        public static Ensurable<string> NotBlank(this Ensurable<string> ensurable, string exceptionMessage = null)
         {
             return ensurable.Satisfies(s => !string.IsNullOrWhiteSpace(s), exceptionMessage ?? "Cannot be null or white space.");
         }
 
-        public static Ensurable<string> HasContent(this Ensurable<string> ensurable, Func<Ensurable<string>, Exception> exceptionFactory)
+        public static Ensurable<string> NotBlank(this Ensurable<string> ensurable, Func<Ensurable<string>, Exception> exceptionFactory)
         {
             return ensurable.Satisfies(s => !string.IsNullOrWhiteSpace(s), exceptionFactory);
         }
